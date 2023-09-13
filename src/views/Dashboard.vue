eadbed8e54dc324f469184d07e67e8f626705400
BrainInAVet <bin3635@gmail.com>
2023-09-13 10:00:44 +0000

    Indent Dashboard.vue

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,54 +1,54 @@
 <script setup lang="ts"></script>
 
 <template>
-	<div class="container">
-		<main class="dashboard">
-			<div class="panel">
-				<div class="title">판매량</div>
-				<div class="typography-box">
-					<div class="typography">50건<span style="font-size: 2rem; color: green">↑</span></div>
-				</div>
-			</div>
-		</main>
-	</div>
+  <div class="container">
+    <main class="dashboard">
+      <div class="panel">
+        <div class="title">판매량</div>
+        <div class="typography-box">
+          <div class="typography">50건<span style="font-size: 2rem; color: green">↑</span></div>
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
 
 <style scoped>
 .panel .title {
-	color: grey;
-	font-size: 1rem;
-	line-height: 1;
+  color: grey;
+  font-size: 1rem;
+  line-height: 1;
 }
 
 .panel .typography-box {
-	position: relative;
-	display: flex;
-	flex: 1 1 auto;
-	margin-bottom: 1rem;
-	justify-content: center;
-	align-items: center;
+  position: relative;
+  display: flex;
+  flex: 1 1 auto;
+  margin-bottom: 1rem;
+  justify-content: center;
+  align-items: center;
 }
 
 .panel .typography {
-	font-size: 3rem;
-	font-weight: bold;
+  font-size: 3rem;
+  font-weight: bold;
 }
 
 .panel {
-	display: flex;
-	flex-direction: column;
-	max-width: 300px;
-	min-height: 300px;
-	background: white;
-	border-radius: 16px;
-  box-shadow: 0px 3px 10px rgba(217,217,217,0.8);
-	padding: 24px;
+  display: flex;
+  flex-direction: column;
+  max-width: 300px;
+  min-height: 300px;
+  background: white;
+  border-radius: 16px;
+  box-shadow: 0px 3px 10px rgba(217, 217, 217, 0.8);
+  padding: 24px;
 }
 
 .dashboard {
-	padding: 40px 0;
-	width: calc(100% - 96px);
-	height: 100%;
-	margin: 0 auto;
+  padding: 40px 0;
+  width: calc(100% - 96px);
+  height: 100%;
+  margin: 0 auto;
 }
 </style>
